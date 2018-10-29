7e7fdd59e3faa41e5c7524f6b8469c81cb943457
Alexander Ng <a_ng9@u.pacific.edu>
2018-10-29 14:49:57 -0700

    Added menu template to fringeoftoday

---
+++ src/fringeoftoday/Displayable.java
@@ -0,0 +1,10 @@
+package fringeoftoday;
+
+import java.util.ArrayList;
+
+import acm.graphics.GObject;
+
+public interface Displayable {
+	public void showContents();
+	public void hideContents();
+}
