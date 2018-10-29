17ffce85f4e4128de6300ffd5b74809aacaa1080
Alex Reynen <alexreynen@gmail.com>
2018-10-29 16:03:29 -0700

    Fixed a lot of spelling goofs Made all the paragraphs and labels needed for tutorial (location pending)

--- src/fringeoftoday/TutorialPane.java
+++ src/fringeoftoday/TutorialPane.java
@@ -1,7 +1,9 @@
 package fringeoftoday;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
 public class TutorialPane extends GraphicsPane {
@@ -9,24 +11,41 @@ public class TutorialPane extends GraphicsPane {
 										// all of the GraphicsProgram calls
 	
 	private GParagraph header;				//The instructions
-	private GParagraph moveInstrucitons;	//Instruction of the keys to use to move
-	private GParagraph atackInstructions;	//Instruction of the keys to use to attack
+	private GParagraph moveInstructions;	//Instructions of the keys to use to move
+	private GParagraph attackInstructions;	//Instructions of the keys to use to attack
+	private GLabel pressToContinue;			//Label instructing the user how to progress
 
 	public TutorialPane(MainApplication app) {
 		this.program = app;
-		header = new GParagraph("Your goal is to defeat all the enemies in a room then progress to the next.\nFind the staircase to the next floor.", 100, 100);
-		header.setFont("Arial-24");		
+		header = new GParagraph("Your goal is to defeat all the enemies in a room then progress to the next.\nFind the staircase to the next floor.", MainApplication.WINDOW_WIDTH/4, 100);
+		header.setFont("Arial-24");	
+		moveInstructions = new GParagraph("To move, use the\n\"WASD\" keys", MainApplication.WINDOW_WIDTH/6, 2*MainApplication.WINDOW_HEIGHT/3);
+		moveInstructions.setFont("Arial-24");	
+		attackInstructions = new GParagraph("To fire, use the \"J\" key\nTo swing your sword,\nuse the \"K\" key", 4*MainApplication.WINDOW_WIDTH/6, 2*MainApplication.WINDOW_HEIGHT/3);
+		attackInstructions.setFont("Arial-24");	
+		pressToContinue = new GLabel("Press any key to continue...", MainApplication.WINDOW_WIDTH/2 - 200, 5*MainApplication.WINDOW_HEIGHT/6);
+		pressToContinue.setFont("Arial-24");	
 	}
 
 	@Override
 	public void showContents() {
 		program.add(header);
-		
+		program.add(moveInstructions);
+		program.add(attackInstructions);
+		program.add(pressToContinue);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(header);
+		program.remove(moveInstructions);
+		program.remove(attackInstructions);
+		program.remove(pressToContinue);
 		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		program.switchToMenu();	//Currently redirects to the main menu, will later go to the game
 	}
 }
