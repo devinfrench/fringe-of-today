c158315bdc966ac8eb9db6f2a82c5dff0aafddc4
Alexander Ng <a_ng9@u.pacific.edu>
2018-10-29 15:50:09 -0700

    Updated MenuPane to include buttons and title

--- src/fringeoftoday/MenuPane.java
+++ src/fringeoftoday/MenuPane.java
@@ -8,30 +8,44 @@ import acm.graphics.GObject;
 public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GButton rect;
-	//private GLabel title;
+	private GButton playButton;
+	private GButton shopButton;
+	private GParagraph title;
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		//title = new GLabel()
-		rect = new GButton("Next", 200, 200, 200, 200);
-		rect.setFillColor(Color.RED);
+		
+		//Title banner
+		title = new GParagraph("Fringe of Today", 150, 300);
+		
+		//Play button
+		playButton = new GButton("Play", 600, 200, 200, 100);
+		playButton.setFillColor(Color.RED);
+		
+		//Shop button
+		shopButton = new GButton("Shop", 600, 400, 200, 100);
+		shopButton.setFillColor(Color.RED);
 	}
 
 	@Override
 	public void showContents() {
-		program.add(rect);
+		program.add(title);
+		program.add(playButton);
+		program.add(shopButton);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(rect);
+		program.remove(playButton);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
+		if (obj == playButton) {
+			//program.switchToGame();
+		}
+		else if (obj == shopButton) {
 			program.switchToShop();
 		}
 	}
