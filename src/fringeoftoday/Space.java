113483c1a3a26cb0dd4a64efccf1fe335ca8d307
Jacob Shour <zenthon127@gmail.com>
2018-10-29 15:18:50 -0700

    Added Space class

---
+++ src/fringeoftoday/Space.java
@@ -0,0 +1,13 @@
+package fringeoftoday;
+
+/**
+ * Class responsible for data stored in each individual space / tile of a room
+ * 
+ * @author Jacob Shour
+ * 
+ */
+public class Space {
+	private int numRow;
+	private int numCol;
+	private SpaceType type;
+}
