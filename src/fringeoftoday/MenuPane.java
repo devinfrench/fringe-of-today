490b9debd3ac45fbfac8970bdca8a6b1f274e354
Alexander Ng <a_ng9@u.pacific.edu>
2018-10-29 16:58:03 -0700

    Renamed buttons to match uml

--- src/fringeoftoday/MenuPane.java
+++ src/fringeoftoday/MenuPane.java
@@ -8,9 +8,9 @@ import acm.graphics.GObject;
 public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GButton playButton;
-	private GButton shopButton;
-	private GButton exitButton;
+	private GButton btnPlay;
+	private GButton btnShop;
+	private GButton btnExit;
 	private GParagraph title;
 	public MenuPane(MainApplication app) {
 		super();
@@ -20,41 +20,41 @@ public class MenuPane extends GraphicsPane {
 		title = new GParagraph("Fringe of Today", 150, 300);
 		
 		//Play button
-		playButton = new GButton("Play", 600, 200, 200, 100);
-		playButton.setFillColor(Color.RED);
+		btnPlay = new GButton("Play", 600, 200, 200, 100);
+		btnPlay.setFillColor(Color.RED);
 		
 		//Shop button
-		shopButton = new GButton("Shop", 600, 400, 200, 100);
-		shopButton.setFillColor(Color.RED);
+		btnShop = new GButton("Shop", 600, 400, 200, 100);
+		btnShop.setFillColor(Color.RED);
 		
 		//Exit button
-		exitButton = new GButton("Exit", 600, 600, 200, 100);
-		shopButton.setFillColor(Color.RED);
+		btnExit = new GButton("Exit", 600, 600, 200, 100);
+		btnShop.setFillColor(Color.RED);
 	}
 
 	@Override
 	public void showContents() {
 		program.add(title);
-		program.add(playButton);
-		program.add(shopButton);
-		program.add(exitButton);
+		program.add(btnPlay);
+		program.add(btnShop);
+		program.add(btnExit);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(title);
-		program.remove(playButton);
-		program.remove(shopButton);
-		program.remove(exitButton);
+		program.remove(btnPlay);
+		program.remove(btnShop);
+		program.remove(btnExit);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == playButton) {
+		if (obj == btnPlay) {
 			//program.switchToGame();
 		}
-		else if (obj == shopButton) {
+		else if (obj == btnShop) {
 			program.switchToShop();
 		}
 	}
