c2a0fd8f143bc94ae18f71b4a499dc311ffd8ea1
Jacob Shour <zenthon127@gmail.com>
2018-10-29 15:21:50 -0700

    Added constructor to Space

--- src/fringeoftoday/Space.java
+++ src/fringeoftoday/Space.java
@@ -10,4 +10,17 @@ public class Space {
 	private int numRow;
 	private int numCol;
 	private SpaceType type;
+	
+	/**
+	 * @param numRow - X position in room 
+	 * @param numCol - Y position in room
+	 * @param type - The type of space; controls behavior
+	 */
+	public Space(int numRow, int numCol, SpaceType type) {
+		super();
+		this.numRow = numRow;
+		this.numCol = numCol;
+		this.type = type;
+	}
+	
 }
