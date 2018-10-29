323c4185eb9bfa65aaf1bb9cebf0e4956dac75c7
Alex Reynen <alexreynen@gmail.com>
2018-10-29 15:38:14 -0700

    Made the tutorial skeleton class

--- src/fringeoftoday/TutorialPane.java
+++ src/fringeoftoday/TutorialPane.java
@@ -7,34 +7,26 @@ import acm.graphics.GObject;
 public class TutorialPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GImage img;
-	private GParagraph para;
+	
+	private GParagraph header;				//The instructions
+	private GParagraph moveInstrucitons;	//Instruction of the keys to use to move
+	private GParagraph atackInstructions;	//Instruction of the keys to use to attack
 
 	public TutorialPane(MainApplication app) {
 		this.program = app;
-		img = new GImage("robot head.jpg", 100, 100);
-		para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
-		para.setFont("Arial-24");
+		header = new GParagraph("Your goal is to defeat all the enemies in a room then progress to the next.\nFind the staircase to the next floor.", 100, 100);
+		header.setFont("Arial-24");		
 	}
 
 	@Override
 	public void showContents() {
-		program.add(img);
-		program.add(para);
+		program.add(header);
+		
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(img);
-		program.remove(para);
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		para.setText("you need\nto click\non the eyes\nto go back");
-		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == img) {
-			program.switchToMenu();
-		}
+		program.remove(header);
+		
 	}
 }
