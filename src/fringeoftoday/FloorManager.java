11ab66be00820dff0c4f3f057987924d1d0c103d
Jacob Shour <zenthon127@gmail.com>
2018-10-31 12:39:01 -0700

    Created FloorManager class

---
+++ src/fringeoftoday/FloorManager.java
@@ -0,0 +1,16 @@
+package fringeoftoday;
+
+import java.util.*;
+
+/**
+ * Class responsible for handling the generation of rooms and floors, 
+ * as well as storing the current floor
+ * 
+ * @author Jacob Shour
+ *
+ */
+public class FloorManager {
+	private ArrayList<char[][]> floorLayouts;
+	private ArrayList<char[][]> roomLayouts;
+	private Floor currentFloor;
+}
