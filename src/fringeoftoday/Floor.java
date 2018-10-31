c430cc24906b7ef1befddec121798ae6c24e3b8a
Jacob Shour <zenthon127@gmail.com>
2018-10-31 11:27:28 -0700

    Added a constructor and getters/setters to Floor

--- src/fringeoftoday/Floor.java
+++ src/fringeoftoday/Floor.java
@@ -9,4 +9,20 @@ package fringeoftoday;
 public class Floor {
 	private Room floorLayout[][];
 	private int levelCount;
+	
+	public Floor(int levelCount) {
+		this.levelCount = levelCount;
+	}
+	
+	public Room getRoom(int row, int col) {
+		return floorLayout[row][col];
+	}
+	
+	public int getLevel() {
+		return levelCount;
+	}
+	
+	public void setRoom(int row, int col, Room r) {
+		floorLayout[row][col] = r;
+	}
 }
