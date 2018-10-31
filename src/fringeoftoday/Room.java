c12978393bf48925225f7509c781588656f2fafd
Jacob Shour <zenthon127@gmail.com>
2018-10-31 10:47:21 -0700

    Added getters and setters to Room

--- src/fringeoftoday/Room.java
+++ src/fringeoftoday/Room.java
@@ -10,4 +10,16 @@ package fringeoftoday;
 public class Room {
 	private Space roomLayout[][];
 	private Exit exits[];
+	
+	public Space getSpace(int row, int col) {
+		return roomLayout[row][col];
+	}
+	
+	public Exit[] getExits() {
+		return exits;
+	}
+	
+	public void setSpace(Space s) {
+		roomLayout[s.getNumRow()][s.getNumCol()] = s;
+	}
 }
