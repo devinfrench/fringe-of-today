3687ff0f56a3ddf47f77638b69094442a9976182
Jacob Shour <zenthon127@gmail.com>
2018-10-31 13:12:56 -0700

    Added a key for .txt room layouts in SpaceType comments

--- src/fringeoftoday/SpaceType.java
+++ src/fringeoftoday/SpaceType.java
@@ -8,13 +8,13 @@ package fringeoftoday;
  * 
  */
 public enum SpaceType {
-	STANDARD, //Standard space
-	IMPASSIBLE, //Space that can be shot over but not walked on
-	COLLIDABLE, //Space that can't be shot over or walked on
-	BASIC_SPAWN, //Appears as a standard space but spawns a basic enemy
-	SNIPER_SPAWN, //Appears as a standard space but spawns a sniper enemy
-	SHOTGUN_SPAWN, //Appears as a standard space but spawns a shotgun enemy
-	STAIRS, //Appears as a standard space but spawns stairs when the room is cleared
-	DOOR, //Warps player to adjacent room; initially locked
-	WALL; //Space on edge of room that can't be shot over or walked on
+	STANDARD,      //'*', Standard space
+	IMPASSIBLE,    //'I', Space that can be shot over but not walked on
+	COLLIDABLE,    //'C', Space that can't be shot over or walked on
+	BASIC_SPAWN,   //'B', Appears as a standard space but spawns a basic enemy
+	SNIPER_SPAWN,  //'N', Appears as a standard space but spawns a sniper enemy
+	SHOTGUN_SPAWN, //'H', Appears as a standard space but spawns a shotgun enemy
+	STAIRS,        //'S', Appears as a standard space but spawns stairs when the room is cleared
+	DOOR,          //'D', Warps player to adjacent room; initially locked
+	WALL;          //'W', Space on edge of room that can't be shot over or walked on
 }
