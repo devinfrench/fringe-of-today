d8bdd486c66083b00ca0880d2fd30a47eb4c259e
Jacob Shour <zenthon127@gmail.com>
2018-10-31 13:09:24 -0700

    Added constants for room size and frameworks for generation functions

--- src/fringeoftoday/FloorManager.java
+++ src/fringeoftoday/FloorManager.java
@@ -10,6 +10,9 @@ import java.util.*;
  *
  */
 public class FloorManager {
+	public static final int NUM_ROOM_ROWS = 11;
+	public static final int NUM_ROOM_COLS = 17;
+	
 	private ArrayList<char[][]> floorLayouts;
 	private ArrayList<char[][]> roomLayouts;
 	private Floor currentFloor;
@@ -24,5 +27,24 @@ public class FloorManager {
 	
 	public void addRoomLayout(char layout[][]) {
 		roomLayouts.add(layout);
+	}
+	
+	public void generateNewFloor() {
+		//TODO Complete function
+		return;
+	}
+	
+	public Room generateRoom(char layout[][]) {
+		Room r = new Room();
+		
+		for (int i = 0; i < NUM_ROOM_ROWS; i++) {
+			for (int j = 0; j < NUM_ROOM_COLS; j++) {
+				switch(layout[i][j]) {
+					//TODO Add switch cases
+				}
+			}
+		}
+		
+		return r;
 	}
 }
