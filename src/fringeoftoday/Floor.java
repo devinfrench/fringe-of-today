9efaddaa45823a9c604aa35f0f4337c4c7ea0011
Jacob Shour <zenthon127@gmail.com>
2018-10-31 11:19:53 -0700

    Added Floor class

---
+++ src/fringeoftoday/Floor.java
@@ -0,0 +1,12 @@
+package fringeoftoday;
+
+/**
+ * Class responsible for holding data on a single floor
+ * 
+ * @author Jacob Shour
+ *
+ */
+public class Floor {
+	private Room floorLayout[][];
+	private int levelCount;
+}
