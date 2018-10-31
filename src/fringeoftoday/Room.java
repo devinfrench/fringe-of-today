0abff10411850dde8735d7052b6510373b546bcf
Jacob Shour <zenthon127@gmail.com>
2018-10-31 10:37:43 -0700

    Created Room class

---
+++ src/fringeoftoday/Room.java
@@ -0,0 +1,13 @@
+package fringeoftoday;
+
+
+/**
+ * Class responsible for holding data on a single room
+ * 
+ * @author Jacob Shour
+ *
+ */
+public class Room {
+	private Space roomLayout[][];
+	private Exit exits[];
+}
