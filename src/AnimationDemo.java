29290b478fbdfeeea2e6e3e067256482bda48754
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-05 14:47:44 -0800

    skeleton code for animation demo

---
+++ src/AnimationDemo.java
@@ -0,0 +1,38 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ * Demonstration of animating an image
+ * 
+ * @author Alexander Ng
+ *
+ */
+public class AnimationDemo extends GraphicsProgram {
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
+	
+	
+	public void run() {
+		
+	}
+	
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+	
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	public void init() {
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		requestFocus();
+	}
+	
+	
+	
+}
