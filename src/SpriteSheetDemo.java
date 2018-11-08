12041abff5bd14be60f4a69784de93e064104336
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-08 13:52:05 -0800

    Removed animationDemo, implemented animation in SpriteSheetDemo

--- src/SpriteSheetDemo.java
+++ src/SpriteSheetDemo.java
@@ -1,25 +1,33 @@
 import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
+import fringeoftoday.entities.Projectile;
+
 import java.awt.*;
 import java.awt.event.*;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.Timer;
+
 /**
  * Demonstration of getting an image from spritesheet
  * 
  * @author Alexander Ng
  *
  */
-public class SpriteSheetDemo extends GraphicsProgram {
+public class SpriteSheetDemo extends GraphicsProgram implements ActionListener {
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
-	public static final int SPRITE_WIDTH = 35;
-	public static final int SPRITE_HEIGHT = 60;
+	public static final int SPRITE_WIDTH = 200;
+	public static final int SPRITE_HEIGHT = 400;
 	public static final String FILE_NAME = "spritesheetdemo.png";
+	public static final int DELAY_MS = 250;
+	
 	private static BufferedImage spriteSheet;
+	private static int frame = 0;
 	private GImage image;
 	public static BufferedImage loadSprite(String file) {
 		BufferedImage sprite = null;
@@ -43,15 +51,25 @@ public class SpriteSheetDemo extends GraphicsProgram {
 	public void run() {
 		image = new GImage(getSprite(0,0), 0, 0);
 		add(image);
+		addMouseListeners();
+		Timer t = new Timer(DELAY_MS, this);
+		t.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (frame == 0) {
+			frame = 2;
+		}
+		else {
+			frame = 0;
+		}
+		image.setImage(getSprite(frame,0));
 	}
 	
 	
 	@Override
-	public void keyPressed(KeyEvent e) {
-	
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
+	public void mousePressed(MouseEvent e) {
 		
 	}
 
