ce76f00dce5bae81691f68d3db74a151ab62ca6f
Devin <devinfrench@users.noreply.github.com>
2018-11-08 17:25:27 -0800

    Update projectile demos

--- src/ProjectileDemo.java
+++ src/ProjectileDemo.java
@@ -1,13 +1,13 @@
-import java.awt.event.MouseEvent;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import javax.swing.Timer;
-
 import acm.graphics.GLabel;
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import fringeoftoday.entities.Projectile;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ProjectileDemo extends GraphicsProgram {
 	
@@ -15,23 +15,24 @@ public class ProjectileDemo extends GraphicsProgram {
     public static final int WINDOW_HEIGHT = 600;
     public static final double VELOCITY = 5.0;
 
-    private Map<GOval, Projectile> projectiles;
+    private List<Projectile> projectiles;
     private GLabel count;
 
     public void run() {
-        projectiles = new ConcurrentHashMap<>();
+        projectiles = new CopyOnWriteArrayList<>();
         count = new GLabel("Projectile Count: 0");
         count.setLocation(20, 20);
         add(count);
         addMouseListeners();
         Timer timer = new Timer(50, e -> {
             count.setLabel("Projectile Count: " + projectiles.size());
-            projectiles.forEach((key, value) -> {
-                if (key.getX() <= 0 || key.getY() <= 0 || key.getY() >= WINDOW_HEIGHT || key.getX() >= WINDOW_WIDTH) {
-                    remove(key);
-                    projectiles.remove(key);
+            projectiles.forEach((p) -> {
+                GObject o = p.getGObject();
+                if (o.getX() <= 0 || o.getY() <= 0 || o.getY() >= WINDOW_HEIGHT || o.getX() >= WINDOW_WIDTH) {
+                    remove(o);
+                    projectiles.remove(p);
                 } else {
-                    key.move(VELOCITY * Math.sin(value.getAngle()), VELOCITY * Math.cos(value.getAngle()));
+                    o.move(VELOCITY * Math.sin(p.getAngle()), VELOCITY * Math.cos(p.getAngle()));
                 }
             });
         });
@@ -42,9 +43,10 @@ public class ProjectileDemo extends GraphicsProgram {
     public void mousePressed(MouseEvent e) {
         if (e.getButton() == 1) {
             Projectile p = new Projectile(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
-            p.setAngle(Math.atan2(e.getX() - p.getPosX(), e.getY() - p.getPosY()));
-            GOval o = new GOval(p.getPosX(), p.getPosY(), 5, 5);
-            projectiles.put(o, p);
+            p.setAngle(Math.atan2(e.getX() - p.getX(), e.getY() - p.getY()));
+            GOval o = new GOval(p.getX(), p.getY(), 5, 5);
+            p.setGObject(o);
+            projectiles.add(p);
             add(o);
         }
     }
--- src/ProjectileMoveDemo.java
+++ src/ProjectileMoveDemo.java
@@ -1,17 +1,17 @@
-import java.awt.Color;
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import fringeoftoday.entities.Projectile;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import javax.swing.Timer;
-
-import acm.graphics.GLabel;
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import fringeoftoday.entities.Projectile;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ProjectileMoveDemo  extends GraphicsProgram implements ActionListener {
 	public static final int WINDOW_WIDTH = 800;
@@ -22,7 +22,7 @@ public class ProjectileMoveDemo  extends GraphicsProgram implements ActionListen
 
 	private GOval ball;
 	public String direction;
-	private Map<GOval, Projectile> projectiles;
+	private List<Projectile> projectiles;
 	private GLabel count;
 
 	public void run() {
@@ -35,19 +35,20 @@ public class ProjectileMoveDemo  extends GraphicsProgram implements ActionListen
 		Timer t = new Timer(BREAK_MS, this);
 		t.start();
 
-		projectiles = new ConcurrentHashMap<>();
+		projectiles = new CopyOnWriteArrayList<>();
 		count = new GLabel("Projectile Count: 0");
 		count.setLocation(20, 20);
 		add(count);
 		addMouseListeners();
 		Timer timer = new Timer(50, e -> {
 			count.setLabel("Projectile Count: " + projectiles.size());
-			projectiles.forEach((key, value) -> {
-				if (key.getX() <= 0 || key.getY() <= 0 || key.getY() >= WINDOW_HEIGHT || key.getX() >= WINDOW_WIDTH) {
-					remove(key);
-					projectiles.remove(key);
+			projectiles.forEach((p) -> {
+				GObject o = p.getGObject();
+				if (o.getX() <= 0 || o.getY() <= 0 || o.getY() >= WINDOW_HEIGHT || o.getX() >= WINDOW_WIDTH) {
+					remove(o);
+					projectiles.remove(p);
 				} else {
-					key.move(VELOCITY * Math.sin(value.getAngle()), VELOCITY * Math.cos(value.getAngle()));
+					o.move(VELOCITY * Math.sin(p.getAngle()), VELOCITY * Math.cos(p.getAngle()));
 				}
 			});
 		});
@@ -97,12 +98,13 @@ public class ProjectileMoveDemo  extends GraphicsProgram implements ActionListen
 	public void mousePressed(MouseEvent e) {
 		if (e.getButton() == 1) {
 			Projectile p = new Projectile(
-					(int) (ball.getX() + ball.getWidth() / 2), 
-					(int) (ball.getY() + ball.getHeight() / 2)
-					);
-			p.setAngle(Math.atan2(e.getX() - p.getPosX(), e.getY() - p.getPosY()));
-			GOval o = new GOval(p.getPosX(), p.getPosY(), 5, 5);
-			projectiles.put(o, p);
+				(int) (ball.getX() + ball.getWidth() / 2),
+				(int) (ball.getY() + ball.getHeight() / 2)
+			);
+			p.setAngle(Math.atan2(e.getX() - p.getX(), e.getY() - p.getY()));
+			GOval o = new GOval(p.getX(), p.getY(), 5, 5);
+			p.setGObject(o);
+			projectiles.add(p);
 			add(o);
 		}
 	}
