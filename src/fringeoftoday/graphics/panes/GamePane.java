e3530ec52f0c6ce3670263cd63e31fe2efe2a49f
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-09 14:51:42 -0800

    Framework for in game header

--- src/fringeoftoday/graphics/panes/GamePane.java
+++ src/fringeoftoday/graphics/panes/GamePane.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 
 import acm.graphics.GObject;
 import acm.graphics.GImage;
+import acm.graphics.GRect;
 import acm.graphics.GLabel;
 import fringeoftoday.MainApplication;
 import fringeoftoday.graphics.GButton;
@@ -15,26 +16,52 @@ public class GamePane extends GraphicsPane {
 										// all of the GraphicsProgram calls
 	public static final int BUTTON_WIDTH = 200;
 	public static final int BUTTON_HEIGHT = 100;
+	public static final int HEADER_WIDTH = MainApplication.WINDOW_WIDTH/3;
+	public static final int HEADER_HEIGHT = 200;
 	private GButton btnPlay;
+	private GRect minimapBox;
+	private GRect infoBox;
+	private GRect healthBox;
+	
 	public GamePane(MainApplication app) {
 		super();
 		program = app;
-		btnPlay = new GButton("Play", (MainApplication.WINDOW_WIDTH - BUTTON_WIDTH) / 2, 400, BUTTON_WIDTH,
-				BUTTON_HEIGHT);
-		btnPlay.setFillColor(Color.BLUE);
-		
+		minimapBox = new GRect(0, 0, HEADER_WIDTH, HEADER_HEIGHT);
+		infoBox = new GRect(HEADER_WIDTH,0,HEADER_WIDTH,HEADER_HEIGHT);
+		healthBox = new GRect(HEADER_WIDTH*2,0,HEADER_WIDTH,HEADER_HEIGHT);
 	}
 
 	@Override
 	public void showContents() {
-		program.add(btnPlay);
+		showHeader();
+		showField();
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(btnPlay);
 	}
-
+	
+	public void showHeader() {
+		program.add(minimapBox);
+		program.add(infoBox);
+		program.add(healthBox);
+	}
+	
+	public void removeHeader() {
+		program.remove(minimapBox);
+		program.remove(infoBox);
+		program.remove(healthBox);
+	}
+	
+	public void showField() {
+		
+	}
+	
+	public void removeField() {
+		
+	}
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
