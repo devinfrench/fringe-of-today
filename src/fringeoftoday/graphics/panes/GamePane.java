161c7ebf5e0d92e3446bb0270a503c13f61f0cae
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-09 15:15:11 -0800

    added boxes for headers and death trigger

--- src/fringeoftoday/graphics/panes/GamePane.java
+++ src/fringeoftoday/graphics/panes/GamePane.java
@@ -18,7 +18,7 @@ public class GamePane extends GraphicsPane {
 	public static final int BUTTON_HEIGHT = 100;
 	public static final int HEADER_WIDTH = MainApplication.WINDOW_WIDTH/3;
 	public static final int HEADER_HEIGHT = 200;
-	private GButton btnPlay;
+	private GButton btnDie;
 	private GRect minimapBox;
 	private GRect infoBox;
 	private GRect healthBox;
@@ -29,17 +29,21 @@ public class GamePane extends GraphicsPane {
 		minimapBox = new GRect(0, 0, HEADER_WIDTH, HEADER_HEIGHT);
 		infoBox = new GRect(HEADER_WIDTH,0,HEADER_WIDTH,HEADER_HEIGHT);
 		healthBox = new GRect(HEADER_WIDTH*2,0,HEADER_WIDTH,HEADER_HEIGHT);
+		btnDie = new GButton("DIE", (MainApplication.WINDOW_WIDTH - BUTTON_WIDTH) / 2, (MainApplication.WINDOW_HEIGHT - BUTTON_HEIGHT) / 2, BUTTON_WIDTH, BUTTON_HEIGHT);
 	}
 
 	@Override
-	public void showContents() {
-		showHeader();
-		showField();
+	public void showContents() {//split showContents into showHeader and showField for clarity
+		showHeader(); //Top bar
+		showField(); //Game field
+		program.add(btnDie);//Testing death screen, remove when things are added
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(btnPlay);
+		removeHeader();
+		removeField();
+		program.remove(btnDie);//Testing death screen, remove when things are added
 	}
 	
 	public void showHeader() {
@@ -62,9 +66,15 @@ public class GamePane extends GraphicsPane {
 		
 	}
 	
+	public void onDeath() {
+		program.switchToDeath();
+	}
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		program.switchToMenu();	//Delete this when things start happening here
+		if (obj == btnDie) {
+			onDeath();
+		}
 	}
 }
