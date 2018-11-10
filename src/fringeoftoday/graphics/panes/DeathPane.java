f7db5df8af5d9843eb6747dc4c1871184858ee77
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-09 16:43:49 -0800

    Added post-death stats to death screen

--- src/fringeoftoday/graphics/panes/DeathPane.java
+++ src/fringeoftoday/graphics/panes/DeathPane.java
@@ -1,29 +1,43 @@
 package fringeoftoday.graphics.panes;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
+import fringeoftoday.graphics.GParagraph;
 import fringeoftoday.MainApplication;
 
 public class DeathPane extends GraphicsPane {
 	private MainApplication program; 
 	
-	private GImage deathscreen; 
+	//TODO: connect variables to actual values
+	private int level = -1;
+	private int coins = -1;
+	private String killer = "a frog";
+	
+	private GImage deathScreen; 
+	private GParagraph deathMessage; 
 
 	public DeathPane(MainApplication app) {
 		this.program = app;
-		deathscreen = new GImage("deathscreen.jpg",0,0);
+		deathScreen = new GImage("deathscreen.jpg",0,0);
+		deathMessage = new GParagraph("Highest level: "+level+" Coins: "+coins+" Killed by: "+killer+"\n\nPress any key to continue", 0,0);
+		deathMessage.setColor(Color.WHITE);
+		deathMessage.setFont("Arial-24");
+		deathMessage.move((MainApplication.WINDOW_WIDTH-deathMessage.getWidth())/2, 600);
 	}
 
 	@Override
 	public void showContents() {
-		program.add(deathscreen);
+		program.add(deathScreen);
+		program.add(deathMessage);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(deathscreen);
+		program.remove(deathScreen);
+		program.remove(deathMessage);
 	}
 
 
