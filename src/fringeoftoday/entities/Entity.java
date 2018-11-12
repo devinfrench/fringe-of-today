23b0e31ac0bc3ce287c1d0356db5483183f6864e
Devin <devinfrench@users.noreply.github.com>
2018-11-11 21:07:40 -0800

    Add documentation

--- src/fringeoftoday/entities/Entity.java
+++ src/fringeoftoday/entities/Entity.java
@@ -13,20 +13,36 @@ public class Entity {
         this.gObj = gObj;
     }
 
+    /**
+     * Gets the x-coordinate of the GObject
+     * @return the x-coordinate of the GObject or 0 if GObject is null
+     */
     public double getX() {
         return gObj != null ? gObj.getX() : 0;
     }
 
+    /**
+     * Sets the x-coordinate of the GObject and updates the GObject's location
+     * @param x the x-coordinate to move to.
+     */
     public void setX(double x) {
         if (gObj != null) {
             gObj.move(x, 0);
         }
     }
 
+    /**
+     * Gets the y-coordinate of the GObject
+     * @return the y-coordinate of the GObject or 0 if GObject is null
+     */
     public double getY() {
         return gObj != null ? gObj.getY() : 0;
     }
 
+    /**
+     * Sets the y-coordinate of the GObject and updates the GObject's location
+     * @param y the x-coordinate to move to.
+     */
     public void setY(double y) {
         if (gObj != null) {
             gObj.move(0, y);
