8cbb8c26229da33fda36052ff10b4a25f717cd6b
Jacob Shour <zenthon127@gmail.com>
2018-11-13 22:24:01 -0800

    Moved pathGenerator to new Direction system

--- src/fringeoftoday/floor/Room.java
+++ src/fringeoftoday/floor/Room.java
@@ -127,30 +127,48 @@ public class Room {
 	}
 	
 	private String pathGenerator(String path, int i, int j, SpaceType type) {
-		//Northwest
-		if (i > 0 && j > 0 && roomLayout[i-1][j-1].getType() == type)
-			path = path + "_nw";
 		//North
 		if (i > 0 && roomLayout[i-1][j].getType() == type)
-			path = path + "_n";
-		//Northeast
-		if (i > 0 && j < ROOM_COLS-1 && roomLayout[i-1][j+1].getType() == type)
-			path = path + "_ne";
-		//West
-		if (j > 0 && roomLayout[i][j-1].getType() == type)
-			path = path + "_w";
-		//East
-		if (j < ROOM_COLS-1 && roomLayout[i][j+1].getType() == type)
-			path = path + "_e";
-		//Southwest
-		if (i < ROOM_ROWS-1 && j > 0 && roomLayout[i+1][j-1].getType() == type)
-			path = path + "_sw";
+			directions.add(Direction.NORTH);
 		//South
 		if (i < ROOM_ROWS-1 && roomLayout[i+1][j].getType() == type)
-			path = path + "_s";
+			directions.add(Direction.SOUTH);
+		//East
+		if (j < ROOM_COLS-1 && roomLayout[i][j+1].getType() == type)
+			directions.add(Direction.EAST);
+		//West
+		if (j > 0 && roomLayout[i][j-1].getType() == type)
+			directions.add(Direction.WEST);
+		//Northeast
+		if (i > 0 && j < ROOM_COLS-1 && roomLayout[i-1][j+1].getType() == type)
+			directions.add(Direction.NORTHEAST);
+		//Northwest
+		if (i > 0 && j > 0 && roomLayout[i-1][j-1].getType() == type)
+			directions.add(Direction.NORTHWEST);
 		//Southeast
-		if (i < ROOM_ROWS-1 && j < ROOM_COLS-1 && roomLayout[i+1][j+1].getType() == type)
-			path = path + "_se";
+		if (i < ROOM_ROWS-1 && j < ROOM_COLS-1  && roomLayout[i+1][j+1].getType() == type)
+			directions.add(Direction.SOUTHEAST);
+		//Southwest
+		if (i < ROOM_ROWS-1 && j > 0 && roomLayout[i+1][j-1].getType() == type)
+			directions.add(Direction.SOUTHWEST);
+		
+		if (directions.contains(Direction.NORTHWEST))
+			path = appendDirection(path, Direction.NORTHWEST);
+		if (directions.contains(Direction.NORTH))
+			path = appendDirection(path, Direction.NORTH);
+		if (directions.contains(Direction.NORTHEAST))
+			path = appendDirection(path, Direction.NORTHEAST);
+		if (directions.contains(Direction.WEST))
+			path = appendDirection(path, Direction.WEST);
+		if (directions.contains(Direction.EAST))
+			path = appendDirection(path, Direction.EAST);
+		if (directions.contains(Direction.SOUTHWEST))
+			path = appendDirection(path, Direction.SOUTHWEST);
+		if (directions.contains(Direction.SOUTH))
+			path = appendDirection(path, Direction.SOUTH);
+		if (directions.contains(Direction.SOUTHEAST))
+			path = appendDirection(path, Direction.SOUTHEAST);
+		
 		
 		return path + ".png";
 	}
