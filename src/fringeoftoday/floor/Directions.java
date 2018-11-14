07081bde695bbfbb5c750cb8964274252e119e39
Jacob Shour <zenthon127@gmail.com>
2018-11-13 22:07:46 -0800

    Added toString to Directions

--- src/fringeoftoday/floor/Directions.java
+++ src/fringeoftoday/floor/Directions.java
@@ -2,4 +2,18 @@ package fringeoftoday.floor;
 
 public enum Directions {
 	NORTH, SOUTH, EAST, WEST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST;
+	
+	public String toString() {
+		switch(this) {
+			case NORTH: return "_n";
+			case SOUTH: return "_s";
+			case EAST: return "_e";
+			case WEST: return "_w";
+			case NORTHEAST: return "_ne";
+			case NORTHWEST: return "_nw";
+			case SOUTHEAST: return "_se";
+			case SOUTHWEST: return "_sw";
+		}
+		return "n/a";
+	}
 }
