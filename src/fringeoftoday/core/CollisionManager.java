8fd15d3543e9de2463708da5bf0dcaa38e55c87c
Devin French <d_french3@u.pacific.edu>
2018-11-16 21:12:02 -0800

    Add collision manager

---
+++ src/fringeoftoday/core/CollisionManager.java
@@ -0,0 +1,36 @@
+package fringeoftoday.core;
+
+import acm.graphics.GObject;
+import fringeoftoday.entities.EntityManager;
+import fringeoftoday.floor.FloorManager;
+import fringeoftoday.floor.Room;
+import fringeoftoday.floor.Space;
+import fringeoftoday.floor.SpaceType;
+
+public class CollisionManager {
+
+    private EntityManager entityManager;
+    private Room room;
+
+    public CollisionManager(EntityManager entityManager, Room room) {
+        this.entityManager = entityManager;
+        this.room = room;
+    }
+
+    public boolean playerCanMove(double x, double y) {
+        GObject player = entityManager.getPlayer().getGObject();
+        x = player.getX() + x + (player.getWidth() / 2);
+        y = player.getY() + y + (player.getHeight() / 2);
+        for (int i = 0; i < FloorManager.ROOM_ROWS; i++) {
+            for (int j = 0; j < FloorManager.ROOM_COLS; j++) {
+                Space space = room.getSpace(i, j);
+                if (space.getGObject().contains(x, y)) {
+                    if (space.getType() != SpaceType.GROUND) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
