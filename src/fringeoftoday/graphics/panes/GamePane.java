12d6baf2fa4de8a7d9b726521551f6a69c0fdbee
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-26 15:05:56 -0800

    changed sprite path

--- src/fringeoftoday/graphics/panes/GamePane.java
+++ src/fringeoftoday/graphics/panes/GamePane.java
@@ -520,19 +520,19 @@ public class GamePane extends GraphicsPane implements ActionListener {
 	private void animatePlayer() {
 		if (player.getIsMoving()) {
 			if(counter%7==0) {
-				((GImage)player.getGObject()).setImage("../media/sprites/player_walking_"+player.getFacing()+"_1.png");
+				((GImage)player.getGObject()).setImage("../media/sprites/player/player_walking_"+player.getFacing()+"_1.png");
 			}
 			else if(counter%7==3){
-				((GImage)player.getGObject()).setImage("../media/sprites/player_standing_"+player.getFacing()+".png");
+				((GImage)player.getGObject()).setImage("../media/sprites/player/player_standing_"+player.getFacing()+".png");
 				
 			}
 			else if(counter%7==6) {
-				((GImage)player.getGObject()).setImage("../media/sprites/player_walking_"+player.getFacing()+"_2.png");
+				((GImage)player.getGObject()).setImage("../media/sprites/player/player_walking_"+player.getFacing()+"_2.png");
 			}
 		}
 		else {
 			
-			((GImage)player.getGObject()).setImage("../media/sprites/player_standing_"+player.getFacing()+".png");
+			((GImage)player.getGObject()).setImage("../media/sprites/player/player_standing_"+player.getFacing()+".png");
 		}
 	}
 	
