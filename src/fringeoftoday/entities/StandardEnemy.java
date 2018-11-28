776aa1ad9e9327bbdbf14b1b9796d9263c768f09
Alexander Ng <a_ng9@u.pacific.edu>
2018-11-28 10:28:55 -0800

    changed base sprite

--- src/fringeoftoday/entities/StandardEnemy.java
+++ src/fringeoftoday/entities/StandardEnemy.java
@@ -17,7 +17,7 @@ public class StandardEnemy extends Enemy {
 
 	public StandardEnemy() {
 		projectileImage = Sprites.loadSprite(PROJECTILE_IMAGE_PATH);
-		Image sprite = Sprites.loadSprite("enemy_sprite_temp.png");
+		Image sprite = Sprites.loadSprite("../media/sprites/pikachu/pikachu_standing_" + this.getFacing() + ".png");
 		setGObject(new GImage(sprite));
 		lastAttackTime = System.currentTimeMillis() + FIRST_ATTACK_DELAY;
 	}
